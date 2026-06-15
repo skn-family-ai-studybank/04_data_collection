fe85e77c070c50ec437e4c7705dc19d8ea2fea41
AhnJung-min <dkswjdals4027@gmail.com>
2026-06-15 17:07:07 +0900

    크롤링

---
+++ naver_news.sql
@@ -0,0 +1,10 @@
+create table naver_news (
+    id int primary key auto_increment,
+    title varchar(512),
+    originallink varchar(512),
+    link varchar(512),
+    description text,
+    pub_date varchar(512),
+    created_at datetime default (current_timestamp)
+);
+
